a50391a73a09fc58f9934f77348a8ad65af7f56b
davit paposhvili <davit paposhvili>
2017-05-02 20:51:57 +0400

    Game version 0.1.1

---
+++ Assets/Screepts/CoinRotor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotor : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    
+    void Update () {
+        transform.Rotate(Vector3.up, rotationSpeed);	
+	}
+
+
+
+
+}
---
+++ Assets/Screepts/Coin_Counter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Coin_Counter : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public string coinStringTag;
+    public string badCoinStringTag;
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision  " + col.gameObject.name);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == coinStringTag)
+        {
+            score++;
+        }
+        else if (other.tag == badCoinStringTag && score != 0)
+        {
+            score--;
+        }
+        Debug.Log("Trigger  " + score + "  " + other.gameObject.name);
+
+        scoreText.text = "Score: " + score;
+        Destroy(other.gameObject);
+    }    
+}
