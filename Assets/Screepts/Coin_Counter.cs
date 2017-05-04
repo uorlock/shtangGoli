61aa92a95fd0df76c5e3bcd1b8cdc10f485d1b14
davit paposhvili <davit paposhvili>
2017-05-04 20:31:00 +0400

    BountyHunter 0.1.1

--- Assets/Screepts/Coin_Counter.cs
+++ Assets/Screepts/Coin_Counter.cs
@@ -7,26 +7,71 @@ public class Coin_Counter : MonoBehaviour
 {
     public int score = 0;
     public Text scoreText;
+
+    public Text resultText;
+    public string winText;
+    public string loseText;
+
     public string coinStringTag;
     public string badCoinStringTag;
+
+    public GameObject Menu;
+    public modzraoba move;
+    public Rigidbody myBody;
+
+    int targetScore;
+
+    void Start()
+    {
+        targetScore = GameObject.FindGameObjectsWithTag(coinStringTag).Length;
+
+        Menu.SetActive(false);
+    }
 
     void OnCollisionEnter(Collision col)
     {
         Debug.Log("Collision  " + col.gameObject.name);
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == coinStringTag)
         {
             score++;
         }
-        else if (other.tag == badCoinStringTag && score != 0)
+        else if (other.tag == badCoinStringTag)
         {
-            score--;
+            GameOver(false);
+
         }
         Debug.Log("Trigger  " + score + "  " + other.gameObject.name);
 
         scoreText.text = "Score: " + score;
         Destroy(other.gameObject);
-    }    
+
+        if (targetScore == score)
+        {
+            GameOver(true);
+        }
+    }
+
+    void GameOver(bool didWin)
+    {
+
+        Menu.SetActive(true);
+        move.enabled = false;
+        myBody.isKinematic = true;
+
+        if (didWin)
+        {
+            resultText.text = winText;
+        }
+
+        else
+        {
+            resultText.text = loseText;
+        }
+    }
+
+
 }
---
+++ Assets/Screepts/MenuButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
